51c542835656151a55b94696123dcfa64b805a34
Sebastien <seb75@fedoraproject.org>
2019-09-05 14:32:11 +0200

    optimize title display

--- archive.php
+++ archive.php
@@ -3,7 +3,6 @@
 	<div class="content">
 		<div class="container">
 
-
 			<div class="row">
 				<div class="col-lg-3">
 					<div class="sticky-top" style="top: 50px;">
@@ -12,6 +11,10 @@
 				</div>
 
 				<div class="col-lg-9">
+
+                    <div>
+                        <h1><?php single_cat_title(); ?></h1>
+                    </div>
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title><?php bloginfo('name'); ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+	<title><?php bloginfo('name'); ?> <?php is_front_page() ? bloginfo('description') : wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('test'); ?>>
