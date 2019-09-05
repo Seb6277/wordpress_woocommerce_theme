20d83ea8bbf558a5f7ffedd2977f139511b59c13
Sebastien <seb75@fedoraproject.org>
2019-09-05 15:54:43 +0200

    optimize single post display

--- single.php
+++ single.php
@@ -17,8 +17,17 @@
                     <img src="<?php the_post_thumbnail_url('post_image'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-5">
 				<?php endif; ?>
 
-				<h1><?php the_title(); ?></h1>
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                    <div class="post-meta">
+                        le <?php the_time('j F Y à H:i'); ?> by
+                        <a href="<?php get_author_posts_url(get_the_author_meta('ID')); ?>">
+                            <?php the_author();?>
+                        </a>
+                    </div>
+
+                    <h1><?php the_title(); ?></h1>
+
 					<?php the_content(); ?>
 				<?php endwhile; endif; ?>
 
